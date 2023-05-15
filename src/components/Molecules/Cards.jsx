581768750586fb726cceb4da91dccfe4cf6fa330
batym24 <baty.mantas@gmail.com>
2023-05-15 15:14:07 +0300

    Created Home and Card components

---
+++ src/components/Molecules/Cards.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+    
+`
+
+const Card = () => {
+    return ( 
+        <>
+        </>
+     );
+}
+ 
+export default Card;
---
+++ src/components/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const StyledMain = styled.main`
+    
+`
+
+const Home = () => {
+    return ( 
+        <StyledMain>
+            
+        </StyledMain>
+     );
+}
+ 
+export default Home;
