adab6033f33915f8ffee221f3deac419e11ac2e6
batym24 <baty.mantas@gmail.com>
2023-05-15 11:10:48 +0300

    created html for form

--- src/components/Pages/Register.jsx
+++ src/components/Pages/Register.jsx
@@ -17,14 +17,47 @@ const Register = () => {
     }
 
     const validationSchema = yup.object({
-        
+
     })
 
     return ( 
         <StyledMain>
             <h1>Registration</h1>
             <form>
-
+                <div>
+                    <label htmlFor="email">Provide your email</label>
+                    <input 
+                    type="email" 
+                    name="email" 
+                    id="email" 
+                    value={''}
+                    onChange={""}
+                    onBlur={""}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Provide your password</label>
+                    <input 
+                    type="password" 
+                    name="password" 
+                    id="password" 
+                    value={''}
+                    onChange={""}
+                    onBlur={""}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="passwordCheck">Repeat the password</label>
+                    <input 
+                    type="password" 
+                    name="passwordCheck" 
+                    id="passwordCheck" 
+                    value={''}
+                    onChange={""}
+                    onBlur={""}
+                    />
+                </div>
+                <input type="submit" value={"Register"} />
             </form>
         </StyledMain>
      );
