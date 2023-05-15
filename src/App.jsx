dcd0d37d8a4062bf452331e1284645697f3a078c
batym24 <baty.mantas@gmail.com>
2023-05-15 11:01:26 +0300

    Imported useFormik hook and yup for form validation and created templates for later code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom'
 import Header from './components/Organisms/Header';
+import Register from './components/Pages/Register';
 
 const App = () => {
   return (
     <>
       <Header />
       <Routes>
-        <Route path='/register'/>
+        <Route path='/register' element={<Register/>}/>
         <Route path='/login'/>
       </Routes>
     </>
--- src/components/Pages/Register.jsx
+++ src/components/Pages/Register.jsx
@@ -1,13 +1,32 @@
 import styled from "styled-components";
+import {useFormik} from 'formik'
+import * as yup from 'yup'
 
 const StyledMain = styled.main`
-    
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `
 
 const Register = () => {
+
+    const values = {
+        email: '',
+        password: '',
+        passwordCheck: ''
+    }
+
+    const validationSchema = yup.object({
+        
+    })
+
     return ( 
-        <>
-        </>
+        <StyledMain>
+            <h1>Registration</h1>
+            <form>
+
+            </form>
+        </StyledMain>
      );
 }
  
