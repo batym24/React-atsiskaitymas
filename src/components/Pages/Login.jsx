ab9e589fe1991e4c3a15101ba240740fb16086b5
batym24 <baty.mantas@gmail.com>
2023-05-15 14:34:38 +0300

    Imported formik and yup, wrote templates for further validation

--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -1,13 +1,37 @@
 import styled from "styled-components";
+import {useFormik} from 'formik'
+import * as yup from 'yup'
 
-const styledMain = styled.main`
+const StyledMain = styled.main`
     
 `
 
 const Login = () => {
+
+    const values = {
+        email: '',
+        password: ''
+    }
+
+    const validationSchema = yup.object({
+        email: yup.string()
+        .email('Enter a valid email')
+        .required('This input is required'),
+        password: yup.string()
+        .matches(
+            /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,20}$/,
+            "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
+          )
+        .required("This input is required")
+    })
+
     return ( 
-        <>
-        </>
+        <StyledMain>
+            <h1>Login</h1>
+            <form>
+
+            </form>
+        </StyledMain>
      );
 }
  
