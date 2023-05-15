f188bfa2a5202cd879ec63abb760ef5de2b47049
batym24 <baty.mantas@gmail.com>
2023-05-15 09:28:30 +0300

    Added html to the Header component

--- src/components/Organisms/Header.jsx
+++ src/components/Organisms/Header.jsx
@@ -1,7 +1,20 @@
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+    
+`
+
 const Header = () => {
     return ( 
-        <>
-        </>
+        <StyledHeader>
+            <div>
+                <img src="https://w7.pngwing.com/pngs/235/872/png-transparent-react-computer-icons-redux-javascript-others-logo-symmetry-nodejs-thumbnail.png" alt="logo" />
+            </div>
+            <div>
+                <button>Login</button>
+                <button>Register</button>
+            </div>
+        </StyledHeader>
      );
 }
  
