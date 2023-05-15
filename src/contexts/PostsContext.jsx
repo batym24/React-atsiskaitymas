eff0cdf1f5aaae43ec0ce65646674e10b7f54894
batym24 <baty.mantas@gmail.com>
2023-05-15 10:40:59 +0300

    Used useReducer and useEffect hooks to retrieve data from local server

--- src/contexts/PostsContext.jsx
+++ src/contexts/PostsContext.jsx
@@ -1,8 +1,23 @@
 import { createContext } from "react";
+import { useReducer, useEffect } from "react";
 
 const PostContext = createContext()
 
+const ACTION_TYPES = {
+    GET: 'getAllPosts'
+}
+
+const reducer = (state, action) => {
+    switch (action.type){
+        case ACTION_TYPES.GET: 
+        return action.data
+    }
+}
+
 const PostProvider = ({children}) => {
+
+    const [posts, setPosts] = useReducer(reducer, []) 
+
     return ( 
         <PostContext.Provider
             value={{
